e00534ab593fc57aa136ea5bc54608a8103f51dc
salamwater <eletilsalam@gmail.com>
2025-04-07 12:13:43 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export const AndroidCompact = () => {
+  return (
+    <div className="android-compact">
+      <div className="div">
+        <div className="text-wrapper">Hello Word</div>
+
+        <div className="rectangle" />
+      </div>
+    </div>
+  );
+};
